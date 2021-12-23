47dae2542340326303d934c52b56f079c1cb0c01
Keeshigan Pirabaharan <spkeeshi@gmail.com>
2021-12-22 22:57:52 -0500

    Dark Mode Option

--- app/src/main/java/com/example/smartwindowsapp/SettingsActivity.kt
+++ app/src/main/java/com/example/smartwindowsapp/SettingsActivity.kt
@@ -1,26 +1,53 @@
 package com.example.smartwindowsapp
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity(), // Next line needed for onSharedPreferenceChanged
+    SharedPreferences.OnSharedPreferenceChangeListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
         if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.settings, SettingsFragment())
-                .commit()
+            supportFragmentManager.beginTransaction().replace(R.id.settings,
+                SettingsFragment()).commit()
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        // Below for Dark Mode
+        PreferenceManager.getDefaultSharedPreferences(this)
+            .registerOnSharedPreferenceChangeListener(this)
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
         }
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        if (key == "dark_mode")
+        {
+            val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+            val dMode = prefs.getBoolean("dark_mode", false)
+            if(dMode)
+            {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            else
+            {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+        }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        PreferenceManager.getDefaultSharedPreferences(this)
+            .unregisterOnSharedPreferenceChangeListener(this)
     }
 }
