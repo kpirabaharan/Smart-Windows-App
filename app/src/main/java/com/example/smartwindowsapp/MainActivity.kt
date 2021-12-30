935f517dcddb901489c245cb7f45323ec6b610bc
Keeshigan Pirabaharan <spkeeshi@gmail.com>
2021-12-29 19:39:35 -0500

    .Mainactivity Update

--- app/src/main/java/com/example/smartwindowsapp/MainActivity.kt
+++ app/src/main/java/com/example/smartwindowsapp/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.smartwindowsapp
 
 import android.content.Intent
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.SeekBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 import androidx.preference.Preference
 import androidx.preference.PreferenceManager
 import com.example.smartwindowsapp.databinding.ActivityMainBinding
@@ -14,7 +18,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding //What is this?
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.settings_menu, menu)
@@ -24,8 +28,7 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId){
             R.id.action_settings -> {
-                val intent = Intent(this,
-                    SettingsActivity::class.java)
+                val intent = Intent(this, SettingsActivity::class.java)
                 startActivity(intent)
             }
         }
@@ -45,16 +48,20 @@ class MainActivity : AppCompatActivity() {
         setCurrentFragment(smartFragment) // Sets current fragments, default fragment smart
 
         bottomNavigationView.setOnNavigationItemSelectedListener{
-            when(it.itemId){
-                R.id.automatic -> setCurrentFragment(autoFragment) // Maps bottom navigation to modes
-                R.id.manual -> setCurrentFragment(manualFragment)
-                R.id.smart -> setCurrentFragment(smartFragment)
+            when(it.itemId){// Maps bottom navigation to modes
+                R.id.automatic -> { setCurrentFragment(autoFragment) }
+                R.id.manual -> { setCurrentFragment(manualFragment) }
+                R.id.smart -> { setCurrentFragment(smartFragment) }
             }
             true
         }
 
         mySettings() // Runs function to retrieve setting values
+
+
     }
+
+
 
     private fun mySettings(){ // Function to get settings values
         val prefs = PreferenceManager.getDefaultSharedPreferences(this)
@@ -62,6 +69,7 @@ class MainActivity : AppCompatActivity() {
         val owner = prefs.getBoolean("primary_owner", false)
         val numUsers = prefs.getInt("user_number", 1)
         val security = prefs.getBoolean("close_option", false)
+        val dMode = prefs.getBoolean("dark_mode", false)
 
         //println(prefs)
         println(signature)
--- app/src/main/java/com/example/smartwindowsapp/ManualFragment.kt
+++ app/src/main/java/com/example/smartwindowsapp/ManualFragment.kt
@@ -1,6 +1,27 @@
 package com.example.smartwindowsapp
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
-class ManualFragment : Fragment(R.layout.fragment_manual){
+class ManualFragment : Fragment(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_manual, container, false)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
